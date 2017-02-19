d8297e264660eb375474ac9e0a0f9ad5bbda3e69
Grzegorz Łoś <grzegorz314@gmail.com>
2017-02-19 17:50:29 +0100

    Redesigning BeerView, part 2

--- src/beershowcase/gui/BeerViewPanel.java
+++ src/beershowcase/gui/BeerViewPanel.java
@@ -3,14 +3,21 @@ package beershowcase.gui;
 
 import beershowcase.beerdata.Beer;
 import beershowcase.beerdata.Brewery;
+import beershowcase.beerdata.StyleKeyword;
 import beershowcase.gui.components.AutoLabel;
+import beershowcase.gui.components.RelativeLayout;
+import beershowcase.utils.FixedPointReal;
+import java.awt.BorderLayout;
+import java.awt.Color;
 import java.awt.Component;
-import java.awt.GridBagConstraints;
-import java.awt.GridBagLayout;
+import java.awt.Font;
 import java.awt.GridLayout;
+import java.awt.Image;
+import java.awt.image.BufferedImage;
 import javax.swing.BorderFactory;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.JScrollPane;
 import javax.swing.JTextArea;
 
 /**
@@ -21,150 +28,148 @@ public class BeerViewPanel extends JPanel {
 
     private final Beer beer;
     private final Brewery brewery;
+    private JPanel beerImageContainer;
+    private JPanel breweryImageContainer;
+    private String currency = "$";
 
     public BeerViewPanel(Beer beer, Brewery brewery) {
         this.beer = beer;
         this.brewery = brewery;
         
-        setLayout(new GridBagLayout());
-        addComp(this, makeColumn1());
-        addComp(this, makeColumn2());
-        addComp(this, makeColumn3());
+        RelativeLayout rl = new RelativeLayout(RelativeLayout.X_AXIS);
+        rl.setFill(true);
+        setLayout(rl);
+        
+        Image img = beer.getLabelImage(RunningApplication.getFileSystem());
+        int h = img.getHeight(null);
+        int w = img.getWidth(null);
+        
+        if (h >= w) {
+            add(makeColumn1ForVertical(), new Float(40));
+            add(makeColumn2ForVertical(), new Float(30));
+            add(makeColumn3ForVertical(), new Float(30));
+        } else {
+            add(makeColumn1ForHorizontal(), new Float(70));
+            add(makeColumn2ForHorizontal(), new Float(30));
+        }
+        
+    }
+
+    private JPanel makeColumn1ForVertical() {
+        JPanel panel = new JPanel();
+        
+        RelativeLayout rl = new RelativeLayout(RelativeLayout.Y_AXIS);
+        rl.setFill(true);
+        
+        panel.setLayout(rl);
+        panel.add(makeNameLabel(), new Float(7));
+        panel.add(makePriceLabel(), new Float(5));
+        panel.add(makeBeerImagePanel(), new Float(88));
+        
+        return panel;
+    }
+
+    private JPanel makeColumn2ForVertical() {
+        JPanel panel = new JPanel();
+        
+        RelativeLayout rl = new RelativeLayout(RelativeLayout.Y_AXIS);
+        rl.setFill(true);
+        
+        panel.setLayout(rl);
+        panel.add(makeStyleLabel(), new Float(5));
+        panel.add(makeParamsPanel(), new Float(15));
+        panel.add( makeDescPanel(), new Float(80));
+        
+        return panel;
+    }
+
+    private JPanel makeColumn3ForVertical() {
+        JPanel panel = new JPanel();
+        
+        RelativeLayout rl = new RelativeLayout(RelativeLayout.Y_AXIS);
+        rl.setFill(true);
+        
+        panel.setLayout(rl);
+        panel.add(makeBreweryNamePanel(), new Float(5));
+        panel.add(makeBreweryImagePanel(), new Float(35));
+        panel.add(makeTagsPanel(), new Float(60));
+        
+        return panel;
     }
     
-    private void addComp(JPanel panel, Pair p) {
-        panel.add(p.comp, p.gbc);
-    }
-
-    private Pair makeColumn1() {
+    private JPanel makeColumn1ForHorizontal() {
+        
         JPanel panel = new JPanel();
-        panel.setLayout(new GridBagLayout());
         
-        addComp(panel, makeNameLabel());
-        addComp(panel, makeStyleLabel());
-        addComp(panel, makeImagePanel());
+        RelativeLayout rl = new RelativeLayout(RelativeLayout.Y_AXIS);
+        rl.setFill(true);
         
-        GridBagConstraints gbc = new GridBagConstraints();
-        gbc.gridx = 0;
-        gbc.gridy = 0;
-        gbc.gridheight = 1;
-        gbc.gridwidth = 1;
-        gbc.fill = GridBagConstraints.BOTH;
-        gbc.anchor = GridBagConstraints.LINE_START;
-        gbc.weightx = 0.4;
-        gbc.weighty = 1.0;
+        panel.setLayout(rl);
+        panel.add(makeRow1ForHorizontal(), new Float(32));
+        panel.add(makeBeerImagePanel(), new Float(68));
         
-        return new Pair(panel, gbc);
-    }
-
-    private Pair makeColumn2() {
-        JPanel panel = new JPanel();
-        panel.setLayout(new GridBagLayout());
-        
-        addComp(panel, makePriceLabel());
-        addComp(panel, makeParamsPanel());
-        addComp(panel, makeDescPanel());
-        
-        GridBagConstraints gbc = new GridBagConstraints();
-        gbc.gridx = 1;
-        gbc.gridy = 0;
-        gbc.gridheight = 1;
-        gbc.gridwidth = 1;
-        gbc.fill = GridBagConstraints.BOTH;
-        gbc.anchor = GridBagConstraints.LINE_START;
-        gbc.weightx = 0.3;
-        gbc.weighty = 1.0;
-        
-        return new Pair(panel, gbc);
-    }
-
-    private Pair makeColumn3() {
-        JPanel panel = new JPanel();
-        panel.setLayout(new GridBagLayout());
-        
-        addComp(panel, makeBreweryNamePanel());
-        addComp(panel, makeBreweryImagePanel());
-        addComp(panel, makeTagsPanel());
-        
-        GridBagConstraints gbc = new GridBagConstraints();
-        gbc.gridx = 2;
-        gbc.gridy = 0;
-        gbc.gridheight = 1;
-        gbc.gridwidth = 1;
-        gbc.fill = GridBagConstraints.BOTH;
-        gbc.anchor = GridBagConstraints.LINE_START;
-        gbc.weightx = 0.3;
-        gbc.weighty = 1.0;
-        
-        return new Pair(panel, gbc);
+        return panel;
     }
     
-    private Pair makeNameLabel() {
+    private JPanel makeRow1ForHorizontal() {
+        JPanel column = new JPanel();
+        RelativeLayout columnLayout = new RelativeLayout(RelativeLayout.Y_AXIS);
+        columnLayout.setFill(true);
+        column.setLayout(columnLayout);
+        column.add(makeNameLabel(), new Float(7.0/32));
+        column.add(makePriceLabel(), new Float(5.0/32));
+        column.add(makeStyleLabel(), new Float(5.0/32));
+        column.add(makeParamsPanel(), new Float(15.0/32));
+        
+        JPanel panel = new JPanel();
+        
+        RelativeLayout rowLayout = new RelativeLayout(RelativeLayout.X_AXIS);
+        rowLayout.setFill(true);
+        panel.setLayout(rowLayout);
+        panel.add(column, new Float(50));
+        panel.add(makeDescPanel(), new Float(50));
+        
+        return panel;
+    }
+    
+    private JPanel makeColumn2ForHorizontal() {
+        return makeColumn3ForVertical();
+    }
+    
+    private Component makeNameLabel() {
         AutoLabel label = new AutoLabel(beer.getName());
+        label.setFontStyle(Font.BOLD);
         
-        GridBagConstraints gbc = new GridBagConstraints();
-        gbc.gridx = 0;
-        gbc.gridy = 0;
-        gbc.gridheight = 1;
-        gbc.gridwidth = 1;
-        gbc.fill = GridBagConstraints.BOTH;
-        gbc.anchor = GridBagConstraints.LINE_START;
-        gbc.weightx = 1.0;
-        gbc.weighty = 0.07;
-        
-        return new Pair(label, gbc);
+        return label;
     }
 
-    private Pair makeStyleLabel() {
-        AutoLabel label = new AutoLabel(beer.getDeclaredStyle());
+    private Component makeStyleLabel() {
+        AutoLabel label = new AutoLabel(beer.getDeclaredStyle(), JLabel.CENTER);
+        label.setFontStyle(Font.ITALIC);
         
-        GridBagConstraints gbc = new GridBagConstraints();
-        gbc.gridx = 0;
-        gbc.gridy = 1;
-        gbc.gridheight = 1;
-        gbc.gridwidth = 1;
-        gbc.fill = GridBagConstraints.BOTH;
-        gbc.anchor = GridBagConstraints.LINE_START;
-        gbc.weightx = 1.0;
-        gbc.weighty = 0.05;
-        
-        return new Pair(label, gbc);
+        return label;
     }
 
-    private Pair makeImagePanel() {
-        JPanel panel = new JPanel();
-        panel.setBorder(BorderFactory.createTitledBorder("Tu bedzie obrazek"));
-        
-        GridBagConstraints gbc = new GridBagConstraints();
-        gbc.gridx = 0;
-        gbc.gridy = 2;
-        gbc.gridheight = 1;
-        gbc.gridwidth = 1;
-        gbc.fill = GridBagConstraints.BOTH;
-        gbc.anchor = GridBagConstraints.LINE_START;
-        gbc.weightx = 1.0;
-        gbc.weighty = 0.88;
-        
-        return new Pair(panel, gbc);
+    private Component makeBeerImagePanel() {
+        beerImageContainer = new JPanel();
+        beerImageContainer.setLayout(new BorderLayout());
+        setBeerImage();
+        return beerImageContainer;
     }
 
-    private Pair makePriceLabel() {
-        AutoLabel label = new AutoLabel("Price: " + beer.getPrice());
+    private Component makePriceLabel() {
+        String price;
+        if (beer.getPrice().equals(new FixedPointReal("0")))
+            price = "?";
+        else
+            price = beer.getPrice() + currency;
         
-        GridBagConstraints gbc = new GridBagConstraints();
-        gbc.gridx = 0;
-        gbc.gridy = 0;
-        gbc.gridheight = 1;
-        gbc.gridwidth = 1;
-        gbc.fill = GridBagConstraints.BOTH;
-        gbc.anchor = GridBagConstraints.LINE_START;
-        gbc.weightx = 1.0;
-        gbc.weighty = 0.05;
+        AutoLabel label = new AutoLabel("Price: " + price);
         
-        return new Pair(label, gbc);
+        return label;
     }
     
-    private Pair makeParamsPanel() {
+    private Component makeParamsPanel() {
         JPanel panel = new JPanel(new GridLayout(3, 2));
         panel.setBorder(BorderFactory.createEtchedBorder());
         
@@ -177,94 +182,68 @@ public class BeerViewPanel extends JPanel {
         panel.add(new AutoLabel("IBU:", JLabel.RIGHT));
         panel.add(new AutoLabel("" + beer.getIbu(), JLabel.LEFT));
         
-        
-        GridBagConstraints gbc = new GridBagConstraints();
-        gbc.gridx = 0;
-        gbc.gridy = 1;
-        gbc.gridheight = 1;
-        gbc.gridwidth = 1;
-        gbc.fill = GridBagConstraints.BOTH;
-        gbc.anchor = GridBagConstraints.LINE_START;
-        gbc.weightx = 1.0;
-        gbc.weighty = 0.15;
-        
-        return new Pair(panel, gbc);
+        return panel;
     }
 
-    private Pair makeDescPanel() {
+    private Component makeDescPanel() {
         JTextArea area = new JTextArea();
         area.setText(beer.getDescritpion(RunningApplication.getFileSystem()));
         area.setLineWrap(true);
+        area.setWrapStyleWord(true);
+        area.setBackground(getBackground());
+        area.setFont(new Font("Purisa", Font.PLAIN, 12));
         
-        GridBagConstraints gbc = new GridBagConstraints();
-        gbc.gridx = 0;
-        gbc.gridy = 2;
-        gbc.gridheight = 1;
-        gbc.gridwidth = 1;
-        gbc.fill = GridBagConstraints.BOTH;
-        gbc.anchor = GridBagConstraints.LINE_START;
-        gbc.weightx = 1.0;
-        gbc.weighty = 0.8;
-        
-        return new Pair(area, gbc);
+        JScrollPane scrollPane = new JScrollPane(area);
+        scrollPane.setBorder(null);
+        return scrollPane;
     }
 
-    private Pair makeBreweryNamePanel() {
-        AutoLabel label = new AutoLabel(brewery.getName());
+    private Component makeBreweryNamePanel() {
+        AutoLabel label = new AutoLabel(brewery.getName(), JLabel.RIGHT);
+        label.setFontStyle(Font.BOLD);
         
-        GridBagConstraints gbc = new GridBagConstraints();
-        gbc.gridx = 0;
-        gbc.gridy = 0;
-        gbc.gridheight = 1;
-        gbc.gridwidth = 1;
-        gbc.fill = GridBagConstraints.BOTH;
-        gbc.anchor = GridBagConstraints.LINE_START;
-        gbc.weightx = 1.0;
-        gbc.weighty = 0.05;
-        
-        return new Pair(label, gbc);
+        return label;
     }
 
-    private Pair makeBreweryImagePanel() {
-        JPanel panel = new JPanel();
-        
-        GridBagConstraints gbc = new GridBagConstraints();
-        gbc.gridx = 0;
-        gbc.gridy = 1;
-        gbc.gridheight = 1;
-        gbc.gridwidth = 1;
-        gbc.fill = GridBagConstraints.BOTH;
-        gbc.anchor = GridBagConstraints.LINE_START;
-        gbc.weightx = 1.0;
-        gbc.weighty = 0.35;
-        
-        return new Pair(panel, gbc);
+    private Component makeBreweryImagePanel() {
+        breweryImageContainer = new JPanel();
+        breweryImageContainer.setLayout(new BorderLayout());
+        setBreweryLogo();
+        return breweryImageContainer;
     }
 
-    private Pair makeTagsPanel() {
-        JPanel panel = new JPanel();
-        panel.setBorder(BorderFactory.createTitledBorder("Tags"));
+    private Component makeTagsPanel() {
+        String bullet = "  \u2022 ";
+        JTextArea area = new JTextArea();
+        for (StyleKeyword keyword: beer.getStyleKeywords())
+            area.append(bullet + keyword.name() + "\n");
+        area.setBackground(getBackground());
         
-        GridBagConstraints gbc = new GridBagConstraints();
-        gbc.gridx = 0;
-        gbc.gridy = 2;
-        gbc.gridheight = 1;
-        gbc.gridwidth = 1;
-        gbc.fill = GridBagConstraints.BOTH;
-        gbc.anchor = GridBagConstraints.LINE_START;
-        gbc.weightx = 1.0;
-        gbc.weighty = 0.6;
-        
-        return new Pair(panel, gbc);
+        JScrollPane scrollPane = new JScrollPane(area);
+        scrollPane.setBorder(BorderFactory.createTitledBorder("Tags"));
+        return scrollPane;
     }
-    
-    private class Pair {
+
+    private void setBreweryLogo() {
+        BufferedImage image = brewery.getLogo(RunningApplication.getFileSystem());
         Component comp;
-        GridBagConstraints gbc;
-
-        public Pair(Component comp, GridBagConstraints gbc) {
-            this.comp = comp;
-            this.gbc = gbc;
+        if (image == null) {
+            comp = new JLabel("No logo");
+        } else {
+            comp = new ImagePanel(image);
         }
+        breweryImageContainer.add(comp);
+    }
+
+    private void setBeerImage() {
+        BufferedImage image = beer.getLabelImage(RunningApplication.getFileSystem());
+        
+        Component comp;
+        if (image == null) {
+            comp = new JLabel("No picture");
+        } else {
+            comp = new ImagePanel(image);
+        }
+        beerImageContainer.add(comp);
     }
 }
--- src/beershowcase/gui/components/AutoLabel.java
+++ src/beershowcase/gui/components/AutoLabel.java
@@ -1,13 +1,9 @@
 
 package beershowcase.gui.components;
 
-import java.awt.Color;
-import java.awt.Component;
 import java.awt.Dimension;
 import java.awt.Font;
 import java.awt.Graphics;
-import java.util.Random;
-import javax.swing.BorderFactory;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 
@@ -18,29 +14,18 @@ import javax.swing.JPanel;
 public class AutoLabel extends JPanel {
     Font baseFont = new Font("Default", Font.PLAIN, 12);
     JLabel renderer = new JLabel();
-    static Random r = new Random();
 
     public AutoLabel() {
         super();
-        setBorder( BorderFactory.createLineBorder(getColor(), 5));
     }
     
     public AutoLabel(String text) {
         renderer.setText(text);
-        setBorder( BorderFactory.createLineBorder(getColor(), 5));
     }
 
     public AutoLabel(String text, int horizontalAlignment) {
         renderer.setText(text);
         renderer.setHorizontalAlignment(horizontalAlignment);
-    }
-    
-    private Color getColor() {
-        return new Color(
-                r.nextInt(256),
-                r.nextInt(256),
-                r.nextInt(256)
-        );
     }
 
     public void setText(String text) {
@@ -59,6 +44,10 @@ public class AutoLabel extends JPanel {
         this.baseFont = baseFont;
     }
     
+    public void setFontStyle(int style) {
+        baseFont = baseFont.deriveFont(style);
+    }
+    
     @Override
     public void paint(Graphics gr) {
         super.paint(gr);
@@ -74,67 +63,5 @@ public class AutoLabel extends JPanel {
             renderer.setFont(font);
         }
         renderer.setSize(getSize());
-        //renderer.setPreferredSize(getSize());
     }
 }
-
-/*
-public class AutoLabel extends JLabel {
-    Font baseFont = new Font("Default", Font.PLAIN, 12);
-    ComponentListener componentListener = new ComponentAdapter() {
-            @Override
-            public void componentResized(ComponentEvent e) {
-                adjustFont();
-            }
-        };
-
-    public AutoLabel() {
-        super();
-        addComponentListener(componentListener);
-    }
-
-    public AutoLabel(Font baseFont) {
-        super();
-        this.baseFont = baseFont;
-        addComponentListener(componentListener);
-    }
-
-    public AutoLabel(String text) {
-        super(text);
-        addComponentListener(componentListener);
-    }
-
-    public AutoLabel(String text, int horizontalAlignment) {
-        super(text, horizontalAlignment);
-        addComponentListener(componentListener);
-    }
-
-    public AutoLabel(Font baseFont, String text) {
-        super(text);
-        this.baseFont = baseFont;
-        addComponentListener(componentListener);
-    }
-
-    public Font getBaseFont() {
-        return baseFont;
-    }
-
-    public void setBaseFont(Font baseFont) {
-        this.baseFont = baseFont;
-    }
-    
-    @Override
-    public Dimension getPreferredSize() {
-        return getSize();
-    }
-    
-    private void adjustFont() {
-        Dimension size = getSize();
-        int fs = size.height / 2;
-        if (baseFont != null) {
-            Font font = new Font(baseFont.getFamily(), baseFont.getStyle(), fs);
-            this.setFont(font);
-        }
-    }
-}
-*/
